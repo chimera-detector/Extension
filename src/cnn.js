b7477a67de28013840d9b47477f24619940654c6
Han_Song <shak5643@gmail.com>
2019-06-02 00:19:39 +0900

    Resolve issue #1

--- src/cnn.js
+++ src/cnn.js
@@ -14,80 +14,84 @@ _gaq.push(['_trackPageview']);
 const cnn_inspection = function (node) {
 
     const ext_headlines = [...node.getElementsByClassName('cd__headline-text')];
+    const inner_headlines = [...node.getElementsByClassName('pg-headline')];
 
-    if (ext_headlines.length > 1) {
-        console.log("Outside of Article");
-        ext_headlines.forEach(function (el) {
-            var headline = el.innerText;
-            var clickbait_req = new XMLHttpRequest();
-            clickbait_req.onreadystatechange = function () {
-                if (clickbait_req.readyState === 4 && clickbait_req.status === 200) {
-                    if (clickbait_req.responseText !== "") {
-                        var data = JSON.parse(clickbait_req.responseText);
-                        var clickbait = data.clickbaitiness;
-                        if (clickbait < 60) {
-                            // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#2ecc71;color:#fff;border-radius:5px'>👍 Not Clickbait</ul>";
-                            // el.insertAdjacentHTML('afterend', html);
-                            console.log("Below 60%");
-                        } else if (clickbait > 90) {
-                            // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#F27935;color:#fff;border-radius:5px'>💁 This is Clickbait</ul>";
-                            // el.insertAdjacentHTML('afterend', html);
-                            console.log("Above 90%");
-                        } else {
-                            // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#e67e22;color:#fff;border-radius:5px'>👻 " + clickbait + "% clickbait</ul>";
-                            // el.insertAdjacentHTML('afterend', html);
-                            console.log("ehh...Dealing");
-                        }
-                    } else {
-                      console.log("Response has empty string");
-                    }
+    if (inner_headlines.length === 1) {
 
-                }
-            };
+      // recognize where the user located outside or inside of article.
+      const inner_headlines = [...node.getElementsByClassName('pg-headline')];
+      let url = window.location.href; // store current location
 
-            // console.log(headline);
-            clickbait_req.open("GET", "https://127.0.0.1:3000/detect?headline=" + headline, true);
-            clickbait_req.send(null);
-        });
+      inner_headlines.forEach(function (el) {
+          var stance_req = new XMLHttpRequest();
+          stance_req.onreadystatechange = function () {
+              if (stance_req.readyState === 4 && stance_req.status === 200) {
+                  if (stance_req.responseText !== "") {
+                      var data = JSON.parse(stance_req.responseText);
+                      var stance = data.stance;
+                      if (stance === "agree") {
+                          // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#2ecc71;color:#fff;border-radius:5px'>👍 Not Clickbait</ul>";
+                          // el.insertAdjacentHTML('afterend', html);
+                          console.log("agree");
+                      } else if (stance === "disagree") {
+                          // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#F27935;color:#fff;border-radius:5px'>💁 This is Clickbait</ul>";
+                            // el.insertAdjacentHTML('afterend', html);
+                          console.log("disagree");
+                      } else if (stance === "discuss") {
+                          // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#e67e22;color:#fff;border-radius:5px'>👻 " + clickbait + "% clickbait</ul>";
+                          // el.insertAdjacentHTML('afterend', html);
+                          console.log("discuss");
+                      } else {
+                          console.log("unrelated");
+                      }
+                  } else {
+                    console.log("Response has empty string");
+                  }
+
+              }
+          };
+
+          // console.log(headline);
+          stance_req.open("GET", "https://127.0.0.1:3000/predict?URL=" + url, true);
+          stance_req.send(null);
+      });
+
     } else {
 
-        // recognize where the user located outside or inside of article.
-        const inner_headlines = [...node.getElementsByClassName('pg-headline')];
-        let url = window.location.href; // store current location
+      ext_headlines.forEach(function (el) {
 
-        inner_headlines.forEach(function (el) {
-            var stance_req = new XMLHttpRequest();
-            stance_req.onreadystatechange = function () {
-                if (stance_req.readyState === 4 && stance_req.status === 200) {
-                    if (stance_req.responseText !== "") {
-                        var data = JSON.parse(stance_req.responseText);
-                        var stance = data.stance;
-                        if (stance === "agree") {
-                            // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#2ecc71;color:#fff;border-radius:5px'>👍 Not Clickbait</ul>";
-                            // el.insertAdjacentHTML('afterend', html);
-                            console.log("agree");
-                        } else if (stance === "disagree") {
-                            // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#F27935;color:#fff;border-radius:5px'>💁 This is Clickbait</ul>";
-                              // el.insertAdjacentHTML('afterend', html);
-                            console.log("disagree");
-                        } else if (stance === "discuss") {
-                            // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#e67e22;color:#fff;border-radius:5px'>👻 " + clickbait + "% clickbait</ul>";
-                            // el.insertAdjacentHTML('afterend', html);
-                            console.log("discuss");
-                        } else {
-                            console.log("unrelated");
-                        }
-                    } else {
-                      console.log("Response has empty string");
-                    }
+          var headline = el.innerText;
+          var clickbait_req = new XMLHttpRequest();
+          clickbait_req.onreadystatechange = function () {
+              if (clickbait_req.readyState === 4 && clickbait_req.status === 200) {
+                  if (clickbait_req.responseText !== "") {
+                      var data = JSON.parse(clickbait_req.responseText);
+                      var clickbait = data.clickbaitiness;
+                      if (clickbait < 60) {
+                          // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#2ecc71;color:#fff;border-radius:5px'>👍 Not Clickbait</ul>";
+                          // el.insertAdjacentHTML('afterend', html);
+                          console.log("Below 60%");
+                      } else if (clickbait > 90) {
+                          // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#F27935;color:#fff;border-radius:5px'>💁 This is Clickbait</ul>";
+                          // el.insertAdjacentHTML('afterend', html);
+                          console.log("Above 90%");
+                      } else {
+                          // let html = "<ul style='position:absolute;top:30px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:#e67e22;color:#fff;border-radius:5px'>👻 " + clickbait + "% clickbait</ul>";
+                          // el.insertAdjacentHTML('afterend', html);
+                          console.log("ehh...Dealing");
+                      }
+                  } else {
+                    console.log("Response has empty string");
+                  }
 
-                }
-            };
+              }
+          };
 
-            // console.log(headline);
-            stance_req.open("GET", "https://127.0.0.1:3000/predict?URL=" + url, true);
-            stance_req.send(null);
-        });
+          // console.log(headline);
+          clickbait_req.open("GET", "https://127.0.0.1:3000/detect?headline=" + headline, true);
+          clickbait_req.send(null);
+      });
+
     }
 
 };
